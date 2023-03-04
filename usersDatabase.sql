1646a8ba0403975faf7eb7937b79585719deb699
B1422 <87228468+B1422@users.noreply.github.com>
2023-03-04 11:51:11 +0700

    Update usersDatabase.sql

--- usersDatabase.sql
+++ usersDatabase.sql
@@ -1,18 +1,30 @@
+CREATE TABLE user_roles(
+	id int primary key,
+	role varchar(20) not null unique
+)
+
+insert into user_roles(id,role) values
+(1,'ADMIN'),
+(2,'CUSTOMER')
+
 CREATE TABLE users (
-	user_role VARCHAR(50) NOT NULL,
+	role_id int not null default 2,
 	user_id int IDENTITY(1,1) NOT NULL,
 	user_username VARCHAR(50) NOT NULL,
 	user_email VARCHAR(50) NOT NULL,
 	user_password CHAR(64) NOT NULL,
 	user_fullName NVARCHAR(50) NOT NULL,
 	UNIQUE(user_userName),
-	PRIMARY KEY (user_role,user_id)
+	PRIMARY KEY (role_id,user_id),
+	constraint fk_roles_users foreign key (role_id) references user_roles(id)
 )
 
 SET IDENTITY_INSERT dbo.users ON 
-INSERT INTO dbo.users(user_role,user_id,user_username,user_email,user_password,user_fullName) VALUES 
-('ADMIN',1,'B1422','thai@gmail.com', '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B', N'Administrator'),
-('CUSTOMER',1,'test','test@gmail.com',  '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B', N'Thai'),
-('Customer',2,'Dummy','lmao@gmail.com', '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B', N'Test')
+INSERT INTO dbo.users(role_id,user_id,user_username,user_email,user_password,user_fullName) VALUES 
+(1,1,'B1422','thai@gmail.com', '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B', N'Administrator'),
+(2,1,'test','test@gmail.com',  '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B', N'Thai'),
+(2,2,'Dummy','lmao@gmail.com', '6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B', N'Test')
 
 SET IDENTITY_INSERT dbo.users OFF
+
+select * from users u inner join user_roles r on u.role_id=r.id
