6f64f4753f876debf6b76279fd588fbafbf460e8
Phạm Xuân Huy <zackwalker130903@gmail.com>
2023-03-20 22:23:27 +0700

    Update paymentDatabase.sql

--- paymentDatabase.sql
+++ paymentDatabase.sql
@@ -18,6 +18,5 @@ primary key(userid,product_id)
 create table user_payment(
 orderid int foreign key references orderlist(orderid) not null,
 userid int foreign key references users(user_id) not null,
-email varchar(50) not null,
-user_address varchar(max)
+payment_type varchar(50) not null
 )
