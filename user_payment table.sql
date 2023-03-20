994def6924bcbb168279ee2801df5c04c0629cca
Phạm Xuân Huy <zackwalker130903@gmail.com>
2023-03-20 22:10:37 +0700

    user_payment table orderid instead va userid

--- user_payment table.sql
+++ user_payment table.sql
@@ -1,6 +1,6 @@
 create table user_payment(
-OrderID int foreign key references orderlist(OrderID) not null,
-username varchar(50) foreign key references users(user_username) not null,
+orderid int foreign key references orderlist(orderid) not null,
+userid int foreign key references users(user_id) not null,
 email varchar(50) not null,
 user_address varchar(max)
 )
