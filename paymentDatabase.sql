a3c715d6407aca843500920b5336061215e5898c
Phạm Xuân Huy <zackwalker130903@gmail.com>
2023-03-20 22:42:37 +0700

    Update paymentDatabase.sql

--- paymentDatabase.sql
+++ paymentDatabase.sql
@@ -1,6 +1,5 @@
 
-	create table orderlist 
-(
+create table orderlist (
 orderid int identity(1,1) not null primary key,
 product_id int  foreign key references products(product_id) ,
 userid int foreign key references users(user_id),
