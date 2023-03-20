2a5f877cdcf6ed556db5e0bbf0b9dbdf8c0cb7bc
Phạm Xuân Huy <zackwalker130903@gmail.com>
2023-03-20 22:12:08 +0700

    update cart (userid, product_id)

--- cart.sql
+++ cart.sql
@@ -1,5 +1,5 @@
 create table cart(
-username VARCHAR(50) NOT NULL,
-product_id int NOT NULL,
-primary key(username,product_id)
+userid int foreign key references users(user_userid),
+product_id int foreign key references products(product_id) NOT NULL,
+primary key(userid,product_id)
 )
