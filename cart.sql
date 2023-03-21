7f51f0d6432c0ea5487fe3c861836b32714d3aef
B1422 <87228468+B1422@users.noreply.github.com>
2023-03-22 01:03:52 +0700

    Update cart.sql

--- cart.sql
+++ cart.sql
@@ -1,6 +1,6 @@
 create table cart(
 userid int foreign key references users(user_userid),
 product_id int foreign key references products(product_id) NOT NULL,
-quantity int,
+quantity int not null,
 primary key(userid,product_id)
 )
