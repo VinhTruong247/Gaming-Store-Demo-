a6efbedb73c16c381fb0d378b640c968f1037557
B1422 <87228468+B1422@users.noreply.github.com>
2023-03-22 01:05:03 +0700

    Update orderlist.sql

--- orderlist.sql
+++ orderlist.sql
@@ -2,7 +2,7 @@ create table orderlist
 (
 orderid int identity(1,1) not null primary key,
 product_id int  foreign key references products(product_id) ,
-userid int foreign key references users(user_id),
+user_id int foreign key references users(user_id),
 cost int ,
 issuedate date
 )
