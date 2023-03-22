5f567661dc4bd0ab2cb7e17f2df37d087c6b54c4
B1422 <87228468+B1422@users.noreply.github.com>
2023-03-22 16:11:24 +0700

    Update paymentDatabase.sql

--- paymentDatabase.sql
+++ paymentDatabase.sql
@@ -11,6 +11,7 @@ issuedate date
 create table cart(
 user_id int foreign key references users(user_id),
 product_id int foreign key references products(product_id) NOT NULL,
+quantity int not null,
 primary key(user_id,product_id)
 )
 
