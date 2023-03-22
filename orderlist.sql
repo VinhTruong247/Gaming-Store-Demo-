32848b5d0268c23414274a2a2c5593a152696273
B1422 <87228468+B1422@users.noreply.github.com>
2023-03-22 16:10:56 +0700

    Delete orderlist.sql

--- orderlist.sql
+++
@@ -1,8 +0,0 @@
-create table orderlist 
-(
-orderid int identity(1,1) not null primary key,
-product_id int  foreign key references products(product_id) ,
-user_id int foreign key references users(user_id),
-cost int ,
-issuedate date
-)
